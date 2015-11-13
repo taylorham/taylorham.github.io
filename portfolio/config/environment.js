e09638ec95a9927b6208166ed749d1cdadb4879d
Taylor Ham <mynameisham@gmail.com>
2015-11-13 15:50:50 -0800

    Changed default routing to 'hash' for subdirectories

--- portfolio/config/environment.js
+++ portfolio/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'portfolio',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
