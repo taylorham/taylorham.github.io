3bd89920c77abfed932644c4e6ff3ad6c17c7f65
Taylor Ham <mynameisham@gmail.com>
2016-01-21 22:33:08 -0800

    portfolio v2.0 initial commit

--- svgManipulator-5d4a510cb30689d6e7416206dd7effb7.js
+++
@@ -1,1 +0,0 @@
-$('img[src$=".svg"]').each(function(){var t=jQuery(this),e=t.attr("id"),r=t.attr("class"),a=t.attr("src");jQuery.get(a,function(a){var s=jQuery(a).find("svg");"undefined"!=typeof e&&(s=s.attr("id",e)),"undefined"!=typeof r&&(s=s.attr("class",r+" replaced-svg")),s=s.removeAttr("xmlns:a"),t.replaceWith(s)},"xml")});
